1a922ee7cf7acb9bf7986102f612822dc6327dc7
robiat <robiatyetunde18@gmail.com>
2025-03-08 12:29:25 +0100

    first javascript practice

---
+++ classwork/script.js
@@ -0,0 +1,15 @@
+function add(a, b){
+    return a + b;
+}
+console.log(add(30, 20))
+
+function multi(x, y){
+    return x * y;
+}
+console.log(multi(2, 6))
+
+function divide(x, y){
+    return x / y;
+}
+console.log(divide(3, 10))
+console.log(divide(1000, 10))
