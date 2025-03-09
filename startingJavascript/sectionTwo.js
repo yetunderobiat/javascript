3842bf9c0b366d61b0eeabbe42221a3604fcca23
robiat <robiatyetunde18@gmail.com>
2025-03-09 12:44:45 +0100

    let, const, var

---
+++ startingJavascript/sectionTwo.js
@@ -0,0 +1,43 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun");
+// console.log(20 + 30 + 40)
+
+// Values and variables
+
+// console.log(20)
+// console.log('robiat')
+
+// let firstName = 'jonas';
+// console.log(firstName)
+
+//values and variables assignments
+// let country = 'Nigeria';
+// let continent = 'africa';
+// let population = 139000000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// data types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 'jonas');
+// console.log(typeof 23);
+
+// undefined variable
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// three different ways of declaring a vriable
+//let
+let age = 30;
+age = 31; //reassigning a value to a variable or we are mutating the age variable. you mutate a variable using let
+
+// const - used to change variables that are not supposed to change at any point in the future. a value in a const  variable cannot be changed.
+
