86eb517c79bfeeb46bee39aa00cc3644ae621bd2
robiat <robiatyetunde18@gmail.com>
2025-03-10 22:02:17 +0100

    coding challenge 1

---
+++ startingJavascript/practical.js
@@ -0,0 +1,26 @@
+// JavaScript Fundamentals – Part 1
+// Coding Challenge #1
+
+//test data 1
+// const markHeight = 1.69;
+// const markMass = 78;
+// const johnHeight = 1.88;
+// const johnMass = 95;
+
+// const markBmi = markMass / (markHeight * 2);
+// console.log('Mark BMI is: ', markBmi);
+
+// const johnBmi = johnMass / (johnHeight * 2);
+// console.log('John BMI is: ', johnBmi);
+
+//test data 2
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76
+
+const markBmi = markMass / markHeight ** 2;
+console.log('Mark BMI is:', markBmi + 'kg')
+
+const johnBmi = johnMass / johnHeight * johnHeight;
+console.log('John BMI is:', johnBmi + 'kg')
