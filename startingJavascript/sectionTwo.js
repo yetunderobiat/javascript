471b2a318a837024981ec52411e2c97d33bf1a34
robiat <robiatyetunde18@gmail.com>
2025-03-11 11:23:48 +0100

    truthy and falsy values

--- startingJavascript/sectionTwo.js
+++ startingJavascript/sectionTwo.js
@@ -143,3 +143,35 @@ console.log('I  am ' + 23 + '  years old') // the '+' operator is converting a n
 console.log(3 + '23' + '10' - 3); // the '-'operator converts a string to number. it does not convert it to string. string - number = number
 console.log('23' * '2'); // string * string = number
 console.log('23' / '2'); // string / string = number
+
+//truthy and falsy values
+
+// in js there are only 5 falsy values 0, '', undefined, null, Nan
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 100;
+if (money){
+    console.log("Don't spend it all")
+}
+else{
+    console.log("You should get a job")
+}
+
+const cash = 0;
+if (cash){
+    console.log("Don't spend it all")
+}
+else{
+    console.log("You should get a job")
+}
+
+let height;
+if (height){
+    console.log('Yay!')
+}
+else{
+    console.log('Noo!')
+}
