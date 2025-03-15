e0d1e3c937a507d1178acaefd00f27d98a9752d5
robiat <robiatyetunde18@gmail.com>
2025-03-15 12:35:06 +0100

    activating string

---
+++ startingJavascript/sectionThree.js
@@ -0,0 +1,12 @@
+// Activating string mode    
+// 'Use strict';
+
+let hasDriversLicense = true;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense)   console.log('I can drive');
+
+const interface = 'audio'
+const private = 234//strict mode reserves these words so we cannot reserve them with variabels
+const if = 'dncjdcn',
