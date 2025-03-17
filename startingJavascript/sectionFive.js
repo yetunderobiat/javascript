6fa19f70b03787b16f4ec5c148a7044f4164ddef
robiat <robiatyetunde18@gmail.com>
2025-03-17 01:42:49 +0100

    section five

--- startingJavascript/sectionFive.js
+++ startingJavascript/sectionFive.js
@@ -3,3 +3,11 @@
 // setting up prettier and vs code
 // installing node.js and setting up a dev environment
 // learning how to code
+// how to think like a developer: become a problem solver!
+    // make sure you 100% understand the problem.
+    // break a big problem into smaller sub - problems
+    // don't be afraid to do as much research as you have to
+    // for bigger problems, write pseudo-code before writing the actual code.
+// Use google, stack overflow and MDN
+// Debugging (fixing problems): a bug is a software defect or problem in a computer program. any unexpected or unintended behaviour of a computer program is a software bug.
+// Debugging with the console and breakpoint
