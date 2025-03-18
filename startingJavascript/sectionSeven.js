696ec537b77edb3acafbbb7cbc9ab54064c619d3
robiat <robiatyetunde18@gmail.com>
2025-03-18 09:10:44 +0100

    selecting and manipulating elements

--- startingJavascript/sectionSeven.js
+++ startingJavascript/sectionSeven.js
@@ -1,6 +1,12 @@
-'use script';
-
-console.log(document.querySelector('.message').textContent);
-
-
 // DOM - document object model. it allows js to access html elements and styles to manipulate them
+"use script";
+
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 2;
+
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector("guess").value);
