244c3bd3e6aaa22968bc058f86826616ad20cc72
robiat <robiatyetunde18@gmail.com>
2025-03-18 12:10:19 +0100

    implementing highscore

--- startingJavascript/sectionSeven.js
+++ startingJavascript/sectionSeven.js
@@ -21,7 +21,8 @@
 // implementing highscore
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 3;
+let score = 5;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -37,6 +38,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (guess > secretNumber) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
@@ -66,7 +72,7 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 
 document.querySelector(".again").addEventListener("click", function () {
-  score = 3;
+  score = 5;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".message").textContent = "Start guessing....";
   document.querySelector(".score").textContent = score;
