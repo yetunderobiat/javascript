3795732bb06e09500161cd79d84072be600fe257
robiat <robiatyetunde18@gmail.com>
2025-03-18 10:52:47 +0100

    implementing the game logic

--- startingJavascript/sectionSeven.js
+++ startingJavascript/sectionSeven.js
@@ -15,16 +15,37 @@
 // console.log(document.querySelector(".guess").value);
 
 // handling the click element
-const x = function () {
-  console.log(33);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+//implementing the game logic
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
+  let score = 20;
 
   if (!guess) {
-    document.querySelector(".message").textContent = "It must be a number";
-    console.log(document.querySelector(".number").textContent);
+    document.querySelector(".message").textContent =
+      "Wrong input, not a number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent =
+      "Yayy, you guessed the number correctly";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Your guess is too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the  game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score < 1) {
+      document.querySelector(".message").textContent = "Your guess is too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the  game";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
-
-
