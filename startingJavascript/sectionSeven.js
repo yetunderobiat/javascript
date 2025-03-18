16bcd45aea58d9f93e808b992d2a2936c2e27675
robiat <robiatyetunde18@gmail.com>
2025-03-18 15:40:55 +0100

    html and css for project 2

--- startingJavascript/sectionSeven.js
+++ startingJavascript/sectionSeven.js
@@ -1,45 +1,6 @@
 // DOM - document object model. it allows js to access html elements and styles to manipulate them
 "use script";
 
-// selecting and manipulating element
-// console.log(document.querySelector(".message").textContent);
-// document.querySelector(".message").textContent = "Correct Number!";
-
-// document.querySelector(".number").textContent = 13;
-// console.log(document.querySelector('.number').textContent);
-
-// document.querySelector(".score").textContent = 2;
-// console.log(document.querySelector(".score").textContent);
-
-// document.querySelector(".guess").value = 23;
-// console.log(document.querySelector(".guess").value);
-
-// when guess is too high
-// else if (guess > secretNumber) {
-// if (score > 1) {
-//   document.querySelector(".message").textContent = "Your guess is too high";
-//   score--;
-//   document.querySelector(".score").textContent = score;
-// } else {
-//   document.querySelector(".message").textContent = "You lost the  game";
-//   document.querySelector(".score").textContent = 0;
-// }
-// document.querySelector("body").style.backgroundColor = "yellow";
-
-// when guess is too low
-//   } else if (guess < secretNumber) {
-//     if (score > 1) {
-//       document.querySelector(".message").textContent = "Your guess is too low";
-//       score--;
-//       document.querySelector(".score").textContent = score;
-//     } else {
-//       document.querySelector(".message").textContent = "You lost the  game";
-//       document.querySelector(".score").textContent = 0;
-//     }
-//     document.querySelector("body").style.backgroundColor = "red";
-//   }
-// });
-
 // handling the click element
 // implementing the game logic
 // manipulating css style
