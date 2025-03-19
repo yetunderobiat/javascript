aaac0493e6cd3a1921f9be11acf52b2c7fb6c60f
robiat <robiatyetunde18@gmail.com>
2025-03-19 04:18:11 +0100

    project

--- startingJavascript/sectionSevenTwo.js
+++ startingJavascript/sectionSevenTwo.js
@@ -5,7 +5,6 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -19,3 +18,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+      closeModal();
+  }
+});
