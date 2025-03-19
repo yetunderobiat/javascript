294b579483a19788c56b1059e352447e8fa7873c
robiat <robiatyetunde18@gmail.com>
2025-03-19 03:54:35 +0100

    working with classes

--- startingJavascript/sectionSevenTwo.js
+++ startingJavascript/sectionSevenTwo.js
@@ -2,7 +2,20 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelector(".show-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnOpenModal.clientHeight; i++) 
-    console.log(btnOpenModal[i].textContent);
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
