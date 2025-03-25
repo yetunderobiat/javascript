c8a828d34f5e7c16c659bccec71765aa36d5f8ba
robiat <robiatyetunde18@gmail.com>
2025-03-25 11:02:41 +0100

    LOGICAL ASSIGNMENT OPERATORS

--- startingJavascript/sectionNine.js
+++ startingJavascript/sectionNine.js
@@ -241,3 +241,33 @@ restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
 // THE NULLISH COALESCING OPERATOR(??)
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+
+// LOGICAL ASSIGNMENT OPERATORS
+const rest1 = {
+  myName: "robby",
+  numGuests: 20,
+};
+const rest2 = {
+  myName: "yetunde",
+  owner: "Giovanni Rossi",
+};
+// OR assignment operator
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests || 10;
+rest2.numGuests || 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+rest1.owner = rest1.owner && "<ANONYMOUS>";
+rest2.owner = rest2.owner && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
