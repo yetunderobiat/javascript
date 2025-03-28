eb09030dffd98bce5207f6b6929d839da7ea3a25
robiat <robiatyetunde18@gmail.com>
2025-03-28 23:31:32 +0100

    quiz

--- classwork/quiz.js
+++ classwork/quiz.js
@@ -31,28 +31,60 @@
 // console.log(numArray);
 // console.log(shallowCopy.push(25));
 
-const eplTeams = [
-  "Arsenal",
-  "Aston Villa",
-  "Bournemouth",
-  "Brentford",
-  "Brighton & Hove Albion",
-  "Burnley",
-  "Chelsea",
-  "Crystal Palace",
-  "Everton",
-  "Fulham",
-  "Liverpool",
-  "Luton Town",
-  "Manchester City",
-  "Manchester United",
-  "Newcastle United",
-  "Nottingham Forest",
-  "Sheffield United",
-  "Tottenham Hotspur",
-  "West Ham United",
-];
+// const eplTeams = [
+//   "Arsenal",
+//   "Aston Villa",
+//   "Bournemouth",
+//   "Brentford",
+//   "Brighton & Hove Albion",
+//   "Burnley",
+//   "Chelsea",
+//   "Crystal Palace",
+//   "Everton",
+//   "Fulham",
+//   "Liverpool",
+//   "Luton Town",
+//   "Manchester City",
+//   "Manchester United",
+//   "Newcastle United",
+//   "Nottingham Forest",
+//   "Sheffield United",
+//   "Tottenham Hotspur",
+//   "West Ham United",
+// ];
 
-for (const num of eplTeams.entries()) {
-  console.log(`Position ${num[0] + 1} is ${num[1]}`);
+// for (const num of eplTeams.entries()) {
+//   console.log(`Position ${num[0] + 1} is ${num[1]}`);
+// }
+
+// Write a for loop that prints numbers from 1 to 20.
+// for (let i = 1; i <= 20; i++) {
+//   console.log(`this is number ${i}`);
+// }
+
+function checkClass(check) {
+  const myCheck = check.slice(-2);
+  if (myCheck === "ng") {
+    console.log("You are a nigerian");
+  } else if (myCheck === "gh") {
+    console.log("you are a ghanian");
+  } else if (myCheck === "us") {
+    console.log("you are an american");
+  } else {
+    console.log("country not determined");
+  }
 }
+checkClass("123yu");
+checkClass("12918us");
+checkClass("98129gh");
+
+// a function that capitalize the input
+function checkInput(myName) {
+  const correctName = myName.trim().toLowerCase();
+  return (
+    correctName[0].toUpperCase() +
+    correctName.slice(1, -1) +
+    correctName[myName.length - 1].toUpperCase()
+  );
+}
+console.log(checkInput("biodun"));
