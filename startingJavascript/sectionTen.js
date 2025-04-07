ed718b81cebf65bd9cabc36b792fbd269705f0e4
robiat <robiatyetunde18@gmail.com>
2025-04-07 23:28:34 +0100

    Functions Accepting Callback Functions

--- startingJavascript/sectionTen.js
+++ startingJavascript/sectionTen.js
@@ -1,50 +1,78 @@
-// Default Parameters
-const bookings = [];
-function createBooking(
-  flightNum,
-  numPassengers = 2,
-  price = 200 * numPassengers
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-}
-createBooking("24er");
-createBooking("hsv", 2, 300);
+// // Default Parameters
+// const bookings = [];
+// function createBooking(
+//   flightNum,
+//   numPassengers = 2,
+//   price = 200 * numPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// }
+// createBooking("24er");
+// createBooking("hsv", 2, 300);
 
-// How Passing Arguments Works: Value vs. Reference
-const flight = "LH234";
-const jonas = {
-  name: "Jonas Schmedtmann",
-  passport: 24739479284,
-};
+// // How Passing Arguments Works: Value vs. Reference
+// const flight = "LH234";
+// const jonas = {
+//   name: "Jonas Schmedtmann",
+//   passport: 24739479284,
+// };
 
-function checkIn(flightNum, passenger) {
-  flightNum = "LH999";
-  passenger.name = "Mr. " + passenger.name;
+// function checkIn(flightNum, passenger) {
+//   flightNum = "LH999";
+//   passenger.name = "Mr. " + passenger.name;
 
-  if (passenger.passport === 24739479284) {
-    alert("Checked in");
-  } else {
-    alert("Wrong passport!");
-  }
-}
+//   if (passenger.passport === 24739479284) {
+//     // alert("Checked in");
+//   } else {
+//     // alert("Wrong passport!");
+//   }
+// }
 
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// function newPassport(person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// }
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
 
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-function newPassport(person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
+// Functions Accepting Callback Functions
+function oneWord(str) {
+  return str.replace(/ /g, "").toLowerCase();
 }
 
-newPassport(jonas);
-checkIn(flight, jonas);
+function upperFirstWord(str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+}
+
+// Higher-order function
+function transformer(str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+function high5() {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
