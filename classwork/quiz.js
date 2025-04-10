1c1b2b5eace19a7dcaa77c414c458c9776e3e968
robiat <robiatyetunde18@gmail.com>
2025-04-10 23:44:34 +0100

    assessments

--- classwork/quiz.js
+++ classwork/quiz.js
@@ -216,21 +216,21 @@
 // discountPrice(60);
 // discountPrice(190);
 // discountPrice(-80);
-function calculateDiscount(amount) {
-  let discount = 0;
+// function calculateDiscount(amount) {
+//   let discount = 0;
 
-  if (amount >= 100) {
-    discount = 20;
-  } else if (amount >= 50) {
-    discount = 10;
-  }
+//   if (amount >= 100) {
+//     discount = 20;
+//   } else if (amount >= 50) {
+//     discount = 10;
+//   }
 
-  return discount;
-}
+//   return discount;
+// }
 
-let purchaseAmount = 85;
-let discount = calculateDiscount(purchaseAmount);
-console.log(`Discount for ₦${purchaseAmount} is ₦${discount}`);
+// let purchaseAmount = 85;
+// let discount = calculateDiscount(purchaseAmount);
+// console.log(`Discount for ₦${purchaseAmount} is ₦${discount}`);
 
 // // // Write a simple number guessing function.
 // // console.log("6. Write a simple number guessing function.");
@@ -252,19 +252,78 @@ console.log(`Discount for ₦${purchaseAmount} is ₦${discount}`);
 
 // A call back function
 
-function getSoap(addWater) {
-  console.log("Get your detergent");
-  addWater();
+// function getSoap(addWater) {
+//   console.log("Get your detergent");
+//   addWater();
+// }
+// function pourWater(startWashing) {
+//   console.log("Add water to the cloths");
+//   startWashing();
+// }
+// function rinseClothe(streadCloth) {
+//   console.log("Start washing your cloth");
+//   streadCloth();
+// }
+// function dryCloth() {
+//   console.log("Rinse cloth");
+// }
+// getSoap(() => pourWater(() => rinseClothe(dryCloth)));
+
+"use strict";
+
+//ILE IYAN ELEWE
+
+// place order
+// prepare food
+//prepare soup
+//add stain
+//select drink
+//food on the way
+//enjoy meal
+
+const menu = {
+  food: ["yam", "semo"],
+  soup: ["efo", "ila", "ewedu"],
+  stain: ["goat meat", "brokoto", "assorted", "ponmo"],
+  drink: ["water", "palm wine", "soft dink"],
+};
+
+function placeOrder(
+  prepare_meal,
+  food_select,
+  soup_select,
+  stain_select,
+  drink_select
+) {
+  setTimeout(() => {
+    console.log(
+      `You have decided to eat ${
+        menu.food[food_select] === "yam" ? "pounded yam" : "semo"
+      }`
+    );
+    prepare_meal(soup_select, stain_select, drink_select);
+  }, 2000);
 }
-function pourWater(startWashing) {
-  console.log("Add water to the cloths");
-  startWashing();
+
+function prepareFood(soup_select, stain_select, drink_select) {
+  setTimeout(() => {
+    console.log("food bn prepared");
+    setTimeout(() => {
+      console.log(`your ${menu.soup[soup_select]} is bn prepared too`);
+      setTimeout(() => {
+        console.log(`you picked ${menu.stain[stain_select]} as accessory`);
+        setTimeout(() => {
+          console.log(`you picked ${menu.drink[drink_select]} as drink`);
+          setTimeout(() => {
+            console.log("Food almost ready");
+            setTimeout(() => {
+              console.log("Enjoy your meal");
+            }, 1000);
+          }, 1000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 1000);
 }
-function rinseClothe(streadCloth) {
-  console.log("Start washing your cloth");
-  streadCloth();
-}
-function dryCloth() {
-  console.log("Rinse cloth");
-}
-getSoap(() => pourWater(() => rinseClothe(dryCloth)));
+
+placeOrder(prepareFood, 1, 1, 2, 0);
