127212f1437c92e481f122f77277521c985c3de6
robiat <robiatyetunde18@gmail.com>
2025-04-11 03:24:32 +0100

    cake company assignment

--- classwork/callback.js
+++ classwork/callback.js
@@ -19,23 +19,86 @@ const cake = {
   text: ["Long life", "Happy Birthday", "A year older", "Hurray"],
   extra: ["icecream", "wine", "fruits", "pizza", "drinks"],
 };
-function orderCake(cakeChoice, bestChoice,select_flavour,select_size) {
+function orderCake(
+  cakeChoice,
+  bestChoice,
+  select_flavour,
+  select_size,
+  select_layers,
+  select_shape,
+  select_color,
+  select_icing,
+  select_topping,
+  select_text,
+  select_extra
+) {
   setTimeout(() => {
     console.log(`My order will be taken from ${cake.location[bestChoice]}`);
-    cakeChoice(select_flavour,select_size);
+    cakeChoice(
+      select_flavour,
+      select_size,
+      select_layers,
+      select_shape,
+      select_color,
+      select_icing,
+      select_topping,
+      select_text,
+      select_extra
+    );
   }, 2000);
 }
-function receiveOrder(select_flavour,select_size) {
+function receiveOrder(
+  select_flavour,
+  select_size,
+  select_layers,
+  select_shape,
+  select_color,
+  select_icing,
+  select_topping,
+  select_text,
+  select_extra
+) {
   setTimeout(() => {
     console.log(`Processing order`);
-    setTimeout(()=>{
-        console.log(`A ${cake.flavour[select_flavour]} cake`);
+    setTimeout(() => {
+      console.log(`A ${cake.flavour[select_flavour]} cake`);
+      setTimeout(() => {
+        console.log(`${cake.size[select_size]} inches in size`);
         setTimeout(() => {
-            console.log(`With ${cake.size[select_size]} inches in size`);
-            
+          console.log(`${cake.layers[select_layers]} layers`);
+          setTimeout(() => {
+            console.log(`${cake.shape[select_shape]} in shape`);
+            setTimeout(() => {
+              console.log(`${cake.color[select_color]} in color`);
+              setTimeout(() => {
+                console.log(`With ${cake.icing[select_icing]} icing on top`);
+                setTimeout(() => {
+                  console.log(`A sprinkle of ${cake.toppings[select_topping]}`);
+                  setTimeout(() => {
+                    console.log(
+                      `A text of ${cake.text[select_text]} written on it`
+                    );
+                    setTimeout(() => {
+                      console.log(
+                        `and a ${cake.extra[select_extra]} reward to the celebrant`
+                      );
+                      setTimeout(() => {
+                        console.log("YOUR FULL ORDER");
+                        setTimeout(() => {
+                          console.log(
+                            `A ${cake.flavour[select_flavour]} cake of ${cake.size[select_size]} inches in size, ${cake.layers[select_layers]} layers, ${cake.shape[select_shape]} in shape, ${cake.color[select_color]} in color, ${cake.icing[select_icing]} icing on top, a sprinkle of ${cake.toppings[select_topping]}, a text of ${cake.text[select_text]} written on it, and a ${cake.extra[select_extra]} reward to the celebrant. HAPPY BIRTHDAY`
+                          );
+                        }, 3000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
         }, 1000);
-        
-    })
-  },1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
 }
-orderCake(receiveOrder, 0, 3, 4);
+orderCake(receiveOrder, 7, 3, 4, 4, 2, 1, 2, 0, 3, 0);
