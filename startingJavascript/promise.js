f9e1b910ec992495e249a7e8151bc7564191e445
robiat <robiatyetunde18@gmail.com>
2025-04-18 23:19:41 +0100

    asyc await

--- startingJavascript/promise.js
+++ startingJavascript/promise.js
@@ -1,21 +1,28 @@
 // https://catfact.ninja/fact
 // https://api.genderize.io?name=temitope
 
-const myCat = "https://catfact.ninja/fact";
-const catFetch = fetch(myCat)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// const myCat = "https://catfact.ninja/fact";
+// const catFetch = fetch(myCat)
+//   .then((response) => response.json())
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
-const myName = "https://api.genderize.io?name=yetunde";
-const nameFetch = fetch(myName)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result);
-  }).catch((error)=>{
-    console.log(error);
-  })
+// const nameFetch = fetch(myName)
+//   .then((response) => response.json())
+//   .then((result) => {
+//     console.log(result);
+//   }).catch((error)=>{
+//     console.log(error);
+//   })
+
+// const myName = "https://api.genderize.io?name=";
+async function checkName() {
+  const result = await fetch("https://api.genderize.io?name=");
+  const data = await result.json();
+  console.log(data);
+}
+checkName();
