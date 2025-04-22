074a30274eaba765f7ca5c6fe294bd724fa8a540
robiat <robiatyetunde18@gmail.com>
2025-04-22 22:55:23 +0100

    assignment

--- startingJavascript/promise.js
+++ startingJavascript/promise.js
@@ -1,27 +1,27 @@
-const promise = new Promise((resolve, reject) => {
-  const randomNumber = Math.floor(Math.random() * 5);
-  console.log(randomNumber);
-  setTimeout(() => {
-    if (randomNumber >= 2) {
-      resolve("You have won");
-    } else {
-      reject("You have lost");
-    }
-  }, 3000);
-});
-console.log(promise);
+// const promise = new Promise((resolve, reject) => {
+//   const randomNumber = Math.floor(Math.random() * 5);
+//   console.log(randomNumber);
+//   setTimeout(() => {
+//     if (randomNumber >= 2) {
+//       resolve("You have won");
+//     } else {
+//       reject("You have lost");
+//     }
+//   }, 3000);
+// });
+// console.log(promise);
 
-const practicePromise = new Promise((resolve, reject) => {
-  const addNumber = 3 + 7;
-  setTimeout(() => {
-    if (addNumber === 10) {
-      resolve("Correct");
-    } else {
-      reject("Wrong");
-    }
-  }, 2000);
-});
-console.log(practicePromise);
+// const practicePromise = new Promise((resolve, reject) => {
+//   const addNumber = 3 + 7;
+//   setTimeout(() => {
+//     if (addNumber === 10) {
+//       resolve("Correct");
+//     } else {
+//       reject("Wrong");
+//     }
+//   }, 2000);
+// });
+// console.log(practicePromise);
 
 // https://catfact.ninja/fact
 // https://api.genderize.io?name=temitope
@@ -36,6 +36,7 @@ console.log(practicePromise);
 //     console.log(error);
 //   });
 
+// const myName = "https://api.example.com/data";
 // const nameFetch = fetch(myName)
 //   .then((response) => response.json())
 //   .then((result) => {
@@ -44,10 +45,44 @@ console.log(practicePromise);
 //     console.log(error);
 //   })
 
-// const myName = "https://api.genderize.io?name=";
 // async function checkName() {
 //   const result = await fetch("https://api.genderize.io?name=");
 //   const data = await result.json();
 //   console.log(data);
 // }
 // checkName();
+
+// const fetchApi = "https://api.genderize.io?name=yetunde";
+// const practiceFetch = fetch(fetchApi)
+//   .then((response) => response.json())
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+// async function checkName() {
+//   const result = await fetch("https://api.genderize.io?name=yetunde")
+//   const data = await result.json()
+//   console.log(data);
+//   console.log(data.name);
+//   console.log(`My  name is ${data.name} i am a ${data.gender}`);
+// }
+// checkName()
+
+async function nameCheck() {
+  try {
+    const result = await fetch("https://api.genderize.io?name=tunde");
+    if (!result.ok) {
+      throw new Error(`HTTP error! Status: ${result.status}`);
+    }
+    const data = await result.json();
+    console.log(data);
+    console.log(data.name);
+    console.log(`My  name is ${data.name} i am a ${data.gender}`);
+  } catch (error) {
+    console.error("There was a problem fetching the data:", error);
+  }
+}
+nameCheck()
