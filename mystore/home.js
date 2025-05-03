2626cd332d716af015c6f851f15c4b0750651635
robiat <robiatyetunde18@gmail.com>
2025-05-04 00:37:30 +0100

    my projet

--- mystore/home.js
+++ mystore/home.js
@@ -1,5 +1,10 @@
-document.querySelector(".top").addEventListener("click", function () {
-  document.querySelector(".navbar-section").scrollIntoView({
-    behavior: "smooth",
+document.addEventListener("DOMContentLoaded", function () {
+  const topButton = document.querySelector(".top");
+  const navbar = document.querySelector(".navbar-section");
+
+  topButton.addEventListener("click", function () {
+    navbar.scrollIntoView({
+      behavior: "smooth",
+    });
   });
 });
