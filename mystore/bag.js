6d67a418f97b79c587ae3c48acd6ef3e5b4b206e
robiat <robiatyetunde18@gmail.com>
2025-05-05 00:20:27 +0100

    my projet

--- mystore/bag.js
+++ mystore/bag.js
@@ -3,3 +3,13 @@ function toggleMenu(button) {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("active");
 }
+
+function toggleForm(type){
+    const signinForm=document.querySelector('.signin-form');
+    const loginForm=document.querySelector('.login-form-box');
+
+    if (type === 'signin'){
+        signinForm.computedStyleMap.display=signinForm.computedStyleMap.display==='block'?'none':'block';
+        loginForm.computedStyleMap.display='none';
+    }
+}
